a84436fb8515f6d368429f1e0984579d0c4b98b1
Juliana Nazare <jules.nazare@gmail.com>
2013-09-14 09:08:29 -0700

    more

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ import flask, flask.views
 import streaming as streaming
 import os
 from flask import session
+from datetime import timedelta
+from flask import make_response, request, current_app
+from functools import update_wrapper
 
 app = flask.Flask(__name__)
 app.secret_key = "SCOPE"
@@ -16,11 +19,52 @@ class View(flask.views.MethodView):
 	def post(self):
 		flask.flash(streaming.stream())
 		return flask.render_template('index.html')
-        
 
 app.add_url_rule('/', view_func=View.as_view('main'), methods=['GET', 'POST'])
 
+def crossdomain(origin=None, methods=None, headers=None,
+				max_age=21600, attach_to_all=True,
+				automatic_options=True):
+	if methods is not None:
+		methods = ', '.join(sorted(x.upper() for x in methods))
+	if headers is not None and not isinstance(headers, basestring):
+		headers = ', '.join(x.upper() for x in headers)
+	if not isinstance(origin, basestring):
+		origin = ', '.join(origin)
+	if isinstance(max_age, timedelta):
+		max_age = max_age.total_seconds()
+
+	def get_methods():
+		if methods is not None:
+			return methods
+
+		options_resp = current_app.make_default_options_response()
+		return options_resp.headers['allow']
+
+	def decorator(f):
+		def wrapped_function(*args, **kwargs):
+			if automatic_options and request.method == 'OPTIONS':
+				resp = current_app.make_default_options_response()
+			else:
+				resp = make_response(f(*args, **kwargs))
+			if not attach_to_all and request.method != 'OPTIONS':
+				return resp
+
+			h = resp.headers
+
+			h['Access-Control-Allow-Origin'] = origin
+			h['Access-Control-Allow-Methods'] = get_methods()
+			h['Access-Control-Max-Age'] = str(max_age)
+			if headers is not None:
+				h['Access-Control-Allow-Headers'] = headers
+			return resp
+
+		f.provide_automatic_options = False
+		return update_wrapper(wrapped_function, f)
+	return decorator
+
+
 if __name__ == '__main__':
-    # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+	# Bind to PORT if defined, otherwise default to 5000.
+	port = int(os.environ.get('PORT', 5000))
+	app.run(host='0.0.0.0', port=port)
